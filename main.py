cfb22b4598d8c1c7ea135d9c26684f1235b003b0
Florian Gutsche <florian.gutsche@physik.hu-berlin.de>
2018-05-23 20:38:22 +0200

    " up 2 date "

--- main.py
+++ main.py
@@ -15,8 +15,10 @@ import numpy as np
 if __name__ == "__main__":
 
     # import setup
-    path_2_setupfile = r"M:\QMR\Abteilungsprojekte\FG\local\masterthesis\setup.txt"
-    path_2_solution = r"M:\QMR\Abteilungsprojekte\FG\local\masterthesis\solutions\impedance_test_3.npy"
+    #path_2_setupfile = r"M:\QMR\Abteilungsprojekte\FG\local\masterthesis\setup.txt"
+    path_2_setupfile = r"setup.txt"
+    #path_2_solution = r"M:\QMR\Abteilungsprojekte\FG\local\masterthesis\solutions\impedance_test_4.npy"
+    path_2_solution = r"solutions/impedance_test_9..npy"
     
     stp = setup( path_2_setupfile )
 
@@ -30,5 +32,5 @@ if __name__ == "__main__":
     # call solver
     current = ImpedanceSolver_m0( stp.I, stp.L, stp.c0, stp.epsilon_m, stp.phi0, stp.T, stp.N, stp.Dt, sol_initial, phiC, stp.DC, stp.DA, stp.epsilon)
     
-    # save output
+    # save output    
     np.save(path_2_solution, current)
