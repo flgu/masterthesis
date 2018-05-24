6343b1db390a191b87dea1d153e19221b6926c65
Florian Gutsche <florian.gutsche@physik.hu-berlin.de>
2018-05-25 01:00:04 +0200

    " ideas for short single sine pertubation"

---
+++ test_environment/functions/residual.py
@@ -0,0 +1,273 @@
+import numpy as np
+import numba as nb
+
+@nb.jit( nb.float64[:]( nb.int32,
+                    nb.float64[:],
+                    nb.float64[:],
+                    nb.float64[:],
+                    nb.float64,
+                    nb.float64,
+                    nb.float64[:],
+                    nb.float64[:],
+                    nb.float64,
+                    nb.float64,
+                    nb.float64,
+                    nb.float64[:],
+                    nb.int32,
+                    nb.float64,
+                    nb.float64,
+                    nb.float64,
+                    nb.float64) )
+def residual( I,
+        Dx,
+        solN,
+        sol1,
+        chi1,
+        chi2,
+        DC,
+        DA,
+        Dt,
+        M,
+        phiC,
+        epsilon,
+        model,
+        kA,
+        foxA,
+        kC,
+        foxC ):
+    '''
+    Stand: 24.05.2018
+
+    Inputs:
+        I = number of control volumes, int32
+        Dx = cell volumes, 1D array float64
+        solN = timestep j solution vector, 1D array float64
+        sol1 = timestep j-1 solution vector, 1D array float64
+        chi1 = numerical constant, scalar, float64
+        chi2 = numerical constant, scalar, float64
+        DC = diffusion constant cations, 1D array float64
+        DA = diffusion constant anions, 1D array float64
+        Dt = constant time step, scalar float64
+        M = nondimensionalization parameter for reference time, scalar float64
+        phiC = catode voltage, scalar float64
+        epsilon = permitivitty vector of solvent, 1D array float64
+        model = declaration of model, scalar int32
+        kA = bondary condition anode, scalar double64
+        foxA = bondary condition anode, scala, double64
+        kC = bondary condition catode, scalar double64
+        foxC = bondary condition catode, scalar double64
+
+    BDF: c_j - c_j-1 + Dt/Dx_i (f_up - f_down)
+
+    previous time step: c_j-1 = sol1view
+
+    Discretization of flux:
+        ( fup - fdown ) / Dx_cell
+
+
+    Cell volume (one dimensional) of cell with index i is Dx[i].
+
+
+    Nondimensionalization:
+    ----------------------
+
+    From nondimensionalozation of the time scale we have a parameter M multiplicated to
+    time step Dt --> M * Dt. This is used to shorten the Impedance simulations.
+
+    '''
+    residual = np.empty(3*I, dtype = np.float64)
+    velup = 0.0
+    veldown = 0.0
+    upfluxC = 0.0
+    upfluxA = 0.0
+    downfluxC = 0.0
+    downfluxA = 0.0
+
+    # cation flux boundary conditions
+    if model == 0:
+        f_A = 0.0
+        f_C = 0.0
+    elif model == 1:
+        f_A = 0.0
+        f_C = 0.0
+    elif model == 2:
+        f_A = 0.0
+        f_C = 0.0
+
+    # Anode, upflux only, substitute downflux with boundary condition
+    # calculate upwinding velocities
+    velup = -chi1*2*(solN[2*I+1] - solN[2*I]) / (Dx[1] + Dx[0])
+
+    # calc upwinding
+    # cations
+    if DC[1] * velup >=0:
+
+        upfluxC = velup * DC[1] * solN[0]
+
+    else:
+
+        upfluxC = velup * DC[1] * solN[1]
+
+    # anions
+    if -DA[1]*velup >=0:
+
+        upfluxA = -velup * DA[1] * solN[I]
+
+    else:
+
+        upfluxA = -velup * DA[1] * solN[I+1]
+
+    # cation
+    residual[0] = ( solN[0] - sol1[0]
+
+    + M * Dt * ( -DC[1] * 2.0 * (solN[1]-solN[0]) / (Dx[0] + Dx[1]) + upfluxC - f_A ) / Dx[0] )
+
+    # anion
+    residual[I] = ( solN[I] - sol1[I]
+
+    + M * Dt * ( -DA[1] * 2 * (solN[I+1]-solN[I]) / (Dx[1] + Dx[0]) + upfluxA ) / Dx[0] )
+
+    # potential at x0 , boundary condition phiA = 0.0 oBdA
+    residual[2*I] = (( 2 * epsilon[1]*(solN[2*I+1]-solN[2*I]) / (Dx[0] + Dx[1])
+
+    - epsilon[0]*solN[2*I] / Dx[0] ) / Dx[0] + chi2*( solN[0] - solN[I]) )
+
+    # inner points, loop over cell centers
+    for i in range(1,I-1):
+
+        #
+        # Godunov scheme with flux limiter
+        #
+
+        # calculate upwinding velocities
+        velup = -chi1 * 2.0 * (solN[2*I+i+1] - solN[2*I+i]) / (Dx[i+1] + Dx[i])
+        veldown = -chi1 * 2.0 * (solN[2*I+i] - solN[2*I+i-1]) / (Dx[i] + Dx[i-1])
+
+        # calculate slope limiter using the previous time step values
+        # Limiter: van Leer
+
+        # cations
+        if (sol1[i] - sol1[i-1]) * (sol1[i+1] - sol1[i]) < 1e-15:
+
+            limiter = 0.0
+
+        else:
+            r = ( sol1[i] - sol1[i-1] ) / ( sol1[i+1] - sol1[i] )
+
+            limiter = 2*r / ( 1.0 + r )
+
+        if velup >=0:
+
+            upfluxC = velup * DC[i+1] * ( solN[i] )
+                        #+ Dx[i] * limiter * ( solN[i+1] - solN[i] ) / (Dx[i+1] + Dx[i]) )
+
+        else:
+
+            upfluxC = velup * DC[i+1] * ( solN[i+1] )
+                #- Dx[i+1]  * limiter * ( solN[i+1] - solN[i] ) / (Dx[i+1] + Dx[i]) )
+
+        if veldown >=0:
+
+            downfluxC = veldown * DC[i] * ( solN[i-1] )
+                   # + Dx[i-1] * limiter * (solN[i] - solN[i-1]) / (Dx[i] + Dx[i-1]) )
+
+        else:
+
+            downfluxC = veldown * DC[i] * ( solN[i] )
+                    #- Dx[i] * limiter * (solN[i] - solN[i-1]) / (Dx[i] + Dx[i-1]) )
+
+        # anions
+        if (sol1[I+i] - sol1[I+i-1]) * (sol1[I+i+1] - sol1[I+i]) < 1e-15:
+
+            limiter = 0.0
+
+        else:
+            r = ( sol1[I+i] - sol1[I+i-1] ) / ( sol1[I+i+1] - sol1[I+i] )
+
+            limiter = 2*r / ( 1.0 + r )
+
+        if -velup >=0:
+
+            upfluxA = -velup * DA[i+1] * ( solN[I+i] )
+                    # + Dx[i] * limiter * ( solN[I+i+1] - solN[I+i] ) / (Dx[i+1] + Dx[i]) )
+        else:
+
+            upfluxA = -velup * DA[i+1] * ( solN[I+i+1] )
+                    #- Dx[i+1]  * limiter * ( solN[I+i+1] - solN[I+i] ) / (Dx[i+1] + Dx[i]) )
+
+        if -veldown >=0:
+
+            downfluxA = -veldown * DA[i] * ( solN[I+i-1] )
+                    #+ Dx[i-1] * limiter * (solN[I+i] - solN[I+i-1]) / (Dx[i] + Dx[i-1]) )
+
+        else:
+
+            downfluxA = -veldown * DA[i] * ( solN[I+i] )
+                    #- Dx[i] * limiter * (solN[I+i] - solN[I+i-1]) / (Dx[i] + Dx[i-1]) )
+
+        # end Godunov / upwinding calculation ----------------------------------
+
+        # cations
+        residual[i] = ( solN[i] - sol1[i]
+
+        + M * Dt * ( -DC[i+1] * 2 * (solN[i+1] - solN[i]) / (Dx[i+1] + Dx[i])
+
+        + DC[i] * 2 * (solN[i] - solN[i-1]) / (Dx[i] + Dx[i-1]) + upfluxC - downfluxC ) / Dx[i] )
+
+        # anions shifted about I
+        residual[I+i] = ( solN[I+i] - sol1[I+i]
+
+        + M * Dt * ( -DA[i+1] * 2 * (solN[I+i+1] - solN[I+i]) / (Dx[i+1] + Dx[i])
+
+        + DA[i] * 2 * (solN[I+i] - solN[I+i-1]) / (Dx[i] + Dx[i-1]) + upfluxA - downfluxA ) / Dx[i])
+
+        # potential equation from 1:I-2 --> two extra calculations needed
+        residual[2*I+i] = ( ( 2 * epsilon[i+1] * (solN[2*I+i+1] - solN[2*I+i]) / (Dx[i+1] + Dx[i])
+
+            -2 * epsilon[i] * (solN[2*I+i] - solN[2*I+i-1]) / (Dx[i] + Dx[i-1]) )
+
+        / Dx[i] + chi2 * ( solN[i] - solN[I+i]) )
+
+    # catode, downflux only, substitute upflux with boundary condition
+    # calc upwinding
+
+    # calculate upwinding velocities
+    veldown = -chi1 * 2 * (solN[3*I-1] - solN[3*I-2]) / (Dx[I-1] + Dx[I-2])
+
+    # cations
+    if DC[I-1] * veldown >=0:
+
+        downfluxC = veldown * solN[I-2] * DC[I-1]
+
+    else:
+
+        downfluxC = veldown * solN[I-1] * DC[I-1]
+
+    # anions
+    if -DA[I-1] * veldown >=0:
+
+        downfluxA = -veldown * solN[2*I-2] * DA[I-1]
+
+    else:
+
+        downfluxA = -veldown * solN[2*I-1] * DA[I-1]
+
+    # catode boundary conditions
+    # cations
+    residual[I-1] = ( solN[I-1] - sol1[I-1]
+
+    + M * Dt * ( DC[I-1] * 2 * (solN[I-1] - solN[I-2]) / (Dx[I-1] + Dx[I-2]) + f_C - downfluxC ) / Dx[I-1] )
+
+    # anions
+    residual[2*I-1] = ( solN[2*I-1] - sol1[2*I-1]
+
+    + M * Dt * ( DA[I-1] * 2 * (solN[2*I-1] - solN[2*I-2]) / (Dx[I-1] + Dx[I-2]) - downfluxA ) / Dx[I-1] )
+
+    # potential at right boundary
+    residual[3*I-1] = ( ( epsilon[I-1] * (phiC - solN[3*I-1]) / Dx[I-1]
+
+        -epsilon[I] * 2 * (solN[3*I-1] - solN[3*I-2]) / (Dx[I-1] + Dx[I-2]) ) / Dx[I-1]
+
+    + chi2*( solN[I-1] - solN[2*I-1]))
+
+    return residual
--- test_environment/main_test.py
+++ test_environment/main_test.py
@@ -12,43 +12,141 @@ from functions.read_setup import setup
 from functions.impedance_functions import createImpedanceVoltage
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+#if __name__ == "__main__":
+
+# import setup
+path_2_setupfile = r"test_setup.txt"
+
+stp = setup( path_2_setupfile )
 
 
-if __name__ == "__main__":
+#%% Create voltage pertubation
 
-    # import setup
-    path_2_setupfile = r"test_setup.txt"
+N = 2024
+M = 1e3
+Dt = 1e-3
+T0 = M * stp.L ** 2 / stp.D0
+
+# create time axis
+time = np.zeros(N, dtype = np.float64)
+for j in range(0,N):
+    
+    time[j] = j * Dt
+    
+    
+# frequency in Hz and amplitude [mV]
+freq = 10
+U0 = 20
 
 
-    stp = setup( path_2_setupfile )
+phiC = np.zeros( N, dtype = np.float64)
 
-    # create voltage
-    phiC = createImpedanceVoltage(stp.N, stp.Dt, stp.T0, stp.phi0, U_offset = 0, num = 40)
-    #phiC = np.ones(stp.N, dtype = np.float64) * 0.01 / stp.phi0
+phiC = (U0 * 1e-3) * np.sin( 2 * np.pi * freq * T0 * time) / stp.phi0
+    
+fig = plt.figure(dpi = 130)
 
-    # initial condition
-    sol_initial = np.zeros([3 * stp.I], dtype = np.float64)
-    sol_initial[0:2*stp.I] = 1.0 / 2.0
+ax = fig.add_subplot(1,1,1)
 
-    # call solver
-    current = ImpedanceSolver_m0( stp.I,
-                                 stp.L,
-                                 stp.c0,
-                                 stp.epsilon_m,
-                                 stp.phi0,
-                                 stp.T,
-                                 stp.N,
-                                 stp.Dt,
-                                 stp.M,
-                                 sol_initial,
-                                 phiC,
-                                 stp.DC,
-                                 stp.DA,
-                                 stp.epsilon,
-                                 stp.model,
-                                 stp.kA,
-                                 stp.foxA,
-                                 stp.kC,
-                                 stp.foxC )
+ax.plot(time, phiC, color = "blue")
+
+plt.show()
+    
+#%%
+
+# initial condition
+sol_initial = np.zeros([3 * stp.I], dtype = np.float64)
+sol_initial[0:2*stp.I] = 1.0 / 2.0
+
+# call solver
+current = ImpedanceSolver_m0( stp.I,
+                             stp.L,
+                             stp.c0,
+                             stp.epsilon_m,
+                             stp.phi0,
+                             stp.T,
+                             N,
+                             Dt,
+                             M,
+                             sol_initial,
+                             phiC,
+                             stp.DC,
+                             stp.DA,
+                             stp.epsilon,
+                             stp.model,
+                             stp.kA,
+                             stp.foxA,
+                             stp.kC,
+                             stp.foxC )
 
     # dont save because its a test file
+
+#%% fitting
+    
+def fit_fun( time, y0, k0, k1 ):
+    global freq, T0
+    
+    return y0 + k0 * np.sin( 2 * np.pi * freq * time *T0 ) + k1 * np.cos( 2 * np.pi * freq * time *T0 )
+
+p_initial = [ U0+1, np.pi]
+
+popt, pcov = curve_fit( fit_fun, time[1:], current[0,1:], p0 = None  )
+
+# calculate parameter
+A = np.sqrt( popt[1] ** 2 + popt[2] ** 2 )
+phase = np.arctan( popt[2] / popt[1] )
+
+print("A", A)
+print("phase", np.rad2deg(shift))
+
+#%%
+# plotting
+
+fig = plt.figure(dpi = 130)
+
+ax = fig.add_subplot(1,1,1)
+
+ax.plot([0, time[-1]], [A,A])
+ax.plot([0, time[-1]], [-A,-A])
+ax.plot(time[1:], phiC[1:], color = "blue")
+ax.plot(time[1:], current[0,1:], color = "red")
+
+# plot fitted curve
+ax.plot(time, fit_fun( time, popt[0], popt[1], popt[2] ), ls = "--", color =  "black")
+
+plt.show()
+
+print(popt)
+
+print(pcov)
+
+fig = plt.figure(dpi = 130)
+
+ax = fig.add_subplot(1,1,1)
+
+ax.plot(phiC, current[0,:], color = "blue")
+#ax.plot(time, , color = "red")
+
+# plot fitted curve
+#ax.plot(time, fit_fun( time, popt[0], popt[1] ), ls = "--", color =  "black")
+
+plt.show()
+
+#%% Calculate complex impedance
+
+Z = np.abs( U0 ) * np.exp(1j * phase) / np.abs(A)
+
+
+fig = plt.figure(dpi = 130)
+
+ax = fig.add_subplot(1,1,1)
+
+ax.plot(Z.real, Z.imag, color = "blue", marker = ".",)
+#ax.plot(time, , color = "red")
+
+# plot fitted curve
+#ax.plot(time, fit_fun( time, popt[0], popt[1] ), ls = "--", color =  "black")
+
+plt.show()
+
